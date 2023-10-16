8016ff64362b566c775e5e7c7e3509deafd25951
Healer Joy <healerjoy@humanmind.me>
2023-10-16 12:35:25 +0000

    Word will be saved from the wordbook for a given position on sequence update.

--- lib/active_kit/sequence/sequence.rb
+++ lib/active_kit/sequence/sequence.rb
@@ -16,48 +16,17 @@ module ActiveKit
         if position
           raise "position '#{position}' is not a valid unsigned integer value greater than 0." unless position.is_a?(Integer) && position > 0
 
-          attribute_at_position = Attribute.where(record_type: record.class.name, name: attribute_name).order(value: :asc).offset(position - 1).limit(1)
-          attribute
-
           wordbook = Wordbook.new
-          total_position_count = Attribute.where(record_type: record.class.name, name: attribute_name).order(value: :asc).count
-          if attribute == attribute_at_position(position)
-            return
-          elsif position == 1
-
-          elsif position == total_position_count
-            wordbook.bookmark = attribute_at_position(total_position_count).value
-
-            attribute.value = wordbook.next_word if wordbook.next_word?
-            attribute.save!
-          elsif position > total_position_count
-            maximum_word = Attribute.where(record_type: record.class.name, name: attribute_name).maximum(:value)
-
-            wordbook.bookmark = maximum_word
-            attribute.value = wordbook.next_word if wordbook.next_word?
-            attribute.save!
-          elsif position < total_position_count
-
-          end
+          word_for_position = wordbook.next_word(count: position)
+          json_where = 'value->"$.sequence.attributes.' + attribute_name.to_s + '" = "' + word_for_position + '"'
+          record_at_position = ActiveKit::Attribute.where(record_type: record.class.name).where(json_where).first&.record
+          record.activekit_association.sequence[:attributes][attribute_name.to_sym] = word_for_position
+          record.activekit_association.save!
+          record_at_position.save! if record_at_position
+        else
+          record.activekit_association.sequence[:attributes][attribute_name.to_sym] = nil
+          record.activekit_association.save!
         end
-      end
-
-      def rebalance_from(position:)
-        ActiveRecord::Base.transaction do
-          wordbook = Wordbook.new
-          Attribute.where(record_type: record.class.name, name: attribute_name).order(value: :asc).offset(position - 1).limit(1)
-          Attribute.where(record_type: record.class.name, name: attribute_name).order(value: :asc).offset(position - 1).each do |attribute|
-            wordbook.bookmark = attribute.value
-            raise "Could not find next word in wordbook while rebalancing" unless wordbook.next_word?
-
-            attribute.value = wordbook.next_word
-            attribute.save!
-          end
-        end
-      end
-
-      def attribute_at_position(position)
-        Attribute.where(record_type: record.class.name, name: attribute_name).order(value: :asc).offset(position - 1).limit(1)
       end
 
       def add_attribute(name:, options:)
