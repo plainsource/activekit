d1255809bf8495c4135891195288afe73d3137e9
Healer Joy <healerjoy@humanmind.me>
2023-10-19 10:47:49 +0000

    Added missing argument to schedule middleware when calling it.

--- lib/active_kit/base/ensure.rb
+++ lib/active_kit/base/ensure.rb
@@ -2,7 +2,7 @@ module ActiveKit
   module Base
     class Ensure
       def self.middleware_for!(request:)
-        ActiveKit::Schedule::Middleware.run
+        ActiveKit::Schedule::Middleware.run(request: request)
       end
 
       def self.setup_for!(current_class:)
