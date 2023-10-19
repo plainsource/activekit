302d20a94ce0071e04fb9a9305175ff177689827
Healer Joy <healerjoy@humanmind.me>
2023-10-19 10:55:13 +0000

    added raise to schedule middleware.

--- lib/active_kit/schedule/middleware.rb
+++ lib/active_kit/schedule/middleware.rb
@@ -2,7 +2,7 @@ module ActiveKit
   module Schedule
     class Middleware
       def self.run(request:)
-        logger.info "coming from middleware of activekit schedule"
+        raise "coming from middleware of activekit schedule."
         # activekit_attribute = ActiveKit::Attribute.where(value: schedule: timestamp < DateTime.now).order(updated_at: :desc)
         # json_where = 'value->"$.schedule.attributes.' + attribute_name.to_s + '" = "' + word_for_position + '"'
         # record_at_position = ActiveKit::Attribute.where(record_type: record.class.name).where(json_where).first&.record
