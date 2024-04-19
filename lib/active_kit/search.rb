169e014470e2e93c61f50e4a259deae00926d32e
Healer Joy <healerjoy@humanmind.me>
2024-04-19 08:54:07 +0000

    Added files to autoload for search feature.

--- lib/active_kit/search.rb
+++ lib/active_kit/search.rb
@@ -2,8 +2,14 @@ module ActiveKit
   module Search
     extend ActiveSupport::Autoload
 
+    autoload :Index
+    autoload :Key
+    autoload :Search
+    autoload :SearchResult
     autoload :Searcher
     autoload :Searching
+    autoload :Suggestion
+    autoload :SuggestionResult
 
     mattr_accessor :redis, instance_accessor: false
   end
